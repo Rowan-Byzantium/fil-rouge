7853a1e746f24cd1ff2ca58632a7c4c29600a048
Rowan Byzantium <verte735@gmail.com>
2023-06-16 16:25:29 +0200

    html + scss + start js

---
+++ js/javascript.js
@@ -0,0 +1,6 @@
+
+const btn1 = document.getElementById('filter-txt');
+const txt1 = document.getElementById('filter-txt');
+btn1.addEventListener('click', function(e) {
+    txt1.classList.toggle('active');
+});
